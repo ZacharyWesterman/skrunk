b2132f516dafa9febd263f95324f3ca8b4c68ace
Zachary Westerman <westerman.zachary@gmail.com>
2022-12-04 20:28:23 -0600

    add helper methods for css manipulation

--- site/js/fields.js
+++ site/js/fields.js
@@ -8,6 +8,12 @@ import Control from './fields/control.js'
 var _ = Template
 _.modal = Modal
 
+_.css = {
+	vars: () => document?.styleSheets[0]?.cssRules[0]?.styleSheet?.rules[0]?.style || [],
+	set_var: (name, value) => document.querySelector(':root').style.setProperty(name, value),
+	get_var: name => getComputedStyle(document.querySelector(':root')).getPropertyValue(name),
+}
+
 //Field control and validation
 var $ = field => (typeof field === 'object') ? field : document.getElementById(field)
 $.val = id => $(id).value
