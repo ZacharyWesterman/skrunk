827e2a0a83e2ada33a18439a9311d7e7b96ea9cf
Zachary Westerman <westerman.zachary@gmail.com>
2022-12-10 16:32:13 -0600

    Add special file upload modal

--- site/js/fields/modal.js
+++ site/js/fields/modal.js
@@ -1,7 +1,7 @@
 var modal = async function(config)
 {
 	await _('modal', config)
-	$('modal-window').style.display = 'block';
+	$('modal-window').style.display = 'block'
 
 	return new Promise((resolve, reject) => {
 		modal.awaiting = {
@@ -19,8 +19,46 @@ modal.cancel = () =>
 
 modal.return = value =>
 {
-	modal.awaiting.resolve(value.toLowerCase())
+	if (typeof value === 'string') value = value.toLowerCase()
+	modal.awaiting.resolve(value)
 	$('modal-window').style.display = 'none';
+}
+
+modal.upload = async function()
+{
+	await _('upload_modal', {})
+	$('modal-upload-window').style.display = 'block'
+
+	return new Promise((resolve, reject) => {
+		modal.awaiting = {
+			resolve: resolve,
+			reject: reject,
+		}
+	})
+}
+
+modal.upload.return = () =>
+{
+	$('modal-upload-window').style.display = 'none'
+}
+
+modal.upload.start = async function()
+{
+	var file = $('modal-file').files[0]
+	$.show('upload-progress')
+
+	await api.upload(file, progress => {
+		const percent = progress.loaded / progress.total
+		$('upload-progress').value = percent
+	})
+
+	await modal({
+		title: 'Success',
+		text: 'Upload complete',
+		buttons: ['OK']
+	}).catch(() => {})
+
+	modal.upload.return()
 }
 
 export default modal
