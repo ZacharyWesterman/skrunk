dad3e71c540221ac12abba270d09d5307c8d252a
Zachary Westerman <westerman.zachary@gmail.com>
2022-12-11 16:14:05 -0600

    Allow non-admins to delete their own blobs

--- application/db/perms.py
+++ application/db/perms.py
@@ -11,32 +11,44 @@ def bad_perms() -> dict:
 		'message': 'You are not qualified to perform this action.',
 	}
 
+def caller_info(info) -> str:
+	token = info.context.headers.get('Authorization', '').split(' ')
+	if len(token) < 2:
+		return None
+
+	username = decode_user_token(token[1]).get('username')
+	if username is None:
+		return None
+
+	userdata = db.data.users.find_one({'username': username})
+	if not userdata:
+		return None
+
+	return userdata
+
+def user_has_perms(user_data: dict, perm_list: list) -> bool:
+	return all(k in user_data['perms'] for k in perm_list)
+
 def require(perms: list, *, perform_on_self: bool = True) -> callable:
 	def inner(method: callable) -> callable:
 		def wrap(_, info, *args, **kwargs):
 			global db
 
-			token = info.context.headers.get('Authorization', '').split(' ')
-			if len(token) < 2:
+			# Make sure the user making the request exists
+			user_data = caller_info(info)
+			if user_data is None:
 				return bad_perms()
-
-			username = decode_user_token(token[1]).get('username', '')
 
 			# Unless otherwise specified,
 			# Ignore credentials if user is editing their own data.
 			if perform_on_self and 'username' in {**kwargs}:
 				other_user = {**kwargs}.get('username')
 
-				if other_user == username:
+				if other_user == user_data['username']:
 					return method(_, info, *args, **kwargs)
 
-			# Make sure user has sufficient perms to perform this action
-			userdata = db.data.users.find_one({'username': username})
-			if not userdata:
-				return bad_perms()
-
 			# If user does not have ALL required perms, fail.
-			if not all(k in userdata['perms'] for k in perms):
+			if not user_has_perms(user_data, perms):
 				return bad_perms()
 
 			return method(_, info, *args, **kwargs)
--- application/resolvers/mutation/blob.py
+++ application/resolvers/mutation/blob.py
@@ -1,10 +1,20 @@
 import application.exceptions as exceptions
-from application.db.blob import delete_blob
+from application.db.blob import delete_blob, get_blob_data
 import application.db.perms as perms
 
-@perms.require(['admin'])
 def resolve_delete_blob(_, info, id: str) -> dict:
 	try:
+		blob_data = get_blob_data(id)
+		user_data = perms.caller_info(info)
+
+		if user_data is None:
+			return perms.bad_perms()
+
+		# Make sure user is either an admin,
+		# or are trying to delete their own blob.
+		if (blob_data.get('creator') != user_data.get('username')) and not perms.user_has_perms(user_data, ['admin']):
+			return perms.bad_perms()
+
 		blob_data = delete_blob(id)
 		blob_data['id'] = blob_data['_id']
 		return { '__typename': 'Blob', **blob_data }
