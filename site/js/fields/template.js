8d06a1d66f74be059315d1738fbc2f35604fde74
Zachary Westerman <westerman.zachary@gmail.com>
2022-12-16 19:45:19 -0600

    Fix _.sync queueing up multiple syncs of the same template

--- site/js/fields/template.js
+++ site/js/fields/template.js
@@ -2,21 +2,7 @@ var __template_map = {}
 
 async function update_dom(name, data, instant = false)
 {
-	var fields = []
-	if (typeof name === 'object')
-	{
-		fields.push(name)
-	}
-	else
-	{
-		var elem = document.getElementById(name)
-		if (elem)
-			fields.push(elem)
-		else
-			fields = document.querySelectorAll('div[name="' + name + '"]')
-	}
-
-	for (var field of fields)
+	for (var field of find_fields(name))
 	{
 		const template_name = field.attributes.template ? field.attributes.template.value : name
 
@@ -34,10 +20,28 @@ async function update_dom(name, data, instant = false)
 	}
 }
 
+function find_fields(name)
+{
+	var fields = []
+	if (typeof name === 'object')
+	{
+		fields.push(name)
+	}
+	else
+	{
+		var elem = document.getElementById(name)
+		if (elem)
+			fields.push(elem)
+		else
+			fields = document.querySelectorAll('div[name="' + name + '"]')
+	}
+	return fields
+}
+
 async function template(template_name, data, instant = false)
 {
 	//show spinner to indicate stuff is loading
-	for (var field of document.querySelectorAll('div[name="' + template_name + '"]'))
+	for (var field of find_fields(template_name))
 	{
 		if (!instant) $.hide(field)
 		field.innerHTML = '<i class="gg-spinner"></i>'
@@ -53,13 +57,31 @@ async function template(template_name, data, instant = false)
 
 //Constantly refresh dom element(s) as long as at least 1 div with the template_name exists.
 //Once it no longer exists, stop refreshing.
+var SyncList = {}
 template.sync = async function(template_name, data_method, frequency = 500, instant = false)
 {
-	if (!(document.querySelectorAll('div[name="' + template_name + '"]').length)) { return }
-	await template(template_name, data_method(), instant)
-	setTimeout(() => {
-		template.sync(template_name, data_method, frequency, instant)
-	}, frequency)
+	const val = SyncList[template_name]
+	SyncList[template_name] = frequency
+
+	const load_func = async () => {
+		if (!(find_fields(template_name).length)) {
+			delete SyncList[template_name]
+			return
+		}
+		await template(template_name, data_method(), instant)
+	}
+
+	if (val !== undefined) {
+		await load_func() //Sync once immediately
+		return //Don't queue more syncing if already syncing
+	}
+
+	const sync_func = async () => {
+		await load_func()
+		setTimeout(() => sync_func(), SyncList[template_name])
+	}
+
+	await sync_func()
 }
 
 export default template
