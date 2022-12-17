3496a48eba2e8ae04a04da46a720c51234144862
Zachary Westerman <westerman.zachary@gmail.com>
2022-12-17 16:02:54 -0600

    default to http, ssl context is opt-in

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 	parser.add_argument('--no-auth', action='store_true')
 	parser.add_argument('--ip', action='store', default='0.0.0.0', type=str)
 	parser.add_argument('--port', action='store', default=5000, type=int)
-	parser.add_argument('--http', action='store_true')
+	parser.add_argument('--https', action='store_true')
 	parser.add_argument('--weather-db', action='store', default='mongodb://localhost:27017/', type=str)
 	parser.add_argument('--data-db', action='store', default='mongodb://localhost:27017/', type=str)
 
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
 	app = application.init(no_auth=args.no_auth, blob_path=args.blob_path, data_db_url=args.data_db, weather_db_url=args.weather_db)
 
-	if args.http:
+	if not args.https:
 		app.run(args.ip, args.port, debug=not args.prod, threaded=True)
 	else:
 		context = ('ssl/cert.pem', 'ssl/privkey.pem')
