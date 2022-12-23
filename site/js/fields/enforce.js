7cd15bb4325537ffcdaa8c8b2babe3075f235fef
Zachary Westerman <westerman.zachary@gmail.com>
2022-12-23 01:42:27 -0600

    don't force trim tags, as they can have spaces in them

--- site/js/fields/enforce.js
+++ site/js/fields/enforce.js
@@ -1,5 +1,5 @@
 export default {
 	id: value => value.replace(/[^\w]/g, '').toLowerCase(),
-	tag: value => value.toLowerCase().trim(),
+	tag: value => value.toLowerCase(),
 	phone: value => value.replace(/[^0-9]/g, ''),
 }
