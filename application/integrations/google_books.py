336ced0345fa859ad05d5fef709c95b72f3a24fe
Zachary Westerman <westerman.zachary@gmail.com>
2023-01-27 20:26:59 -0600

    use book id (bugfix)

--- application/integrations/google_books.py
+++ application/integrations/google_books.py
@@ -61,7 +61,7 @@ def query(*, title: str = '', author: str = '') -> list:
 def get(*, id: str) -> dict:
 	response_fields = 'items(id,volumeInfo(authors,title,subtitle,description,industryIdentifiers,pageCount,categories,maturityRating,language,publisher,publishedDate,imageLinks))'
 
-	url = f'https://www.googleapis.com/books/v1/volumes?q={text_query}&key={API_KEY}&fields={response_fields}'
+	url = f'https://www.googleapis.com/books/v1/volumes?id={id}&key={API_KEY}&fields={response_fields}'
 	response = requests.get(url)
 	if response.status_code != 200:
 		raise exceptions.ApiFailedError(f'API call failed with status code {response.status_code}')
