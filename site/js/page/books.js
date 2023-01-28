ffa38da4ce82e9bed00b371a996d7668bf2d94d4
Zachary Westerman <westerman.zachary@gmail.com>
2023-01-27 20:19:40 -0600

    Add basic book scanning logic

---
+++ site/js/page/books.js
@@ -0,0 +1,55 @@
+//Start the NFC reader ONCE per session, and don't stop it.
+//Trying to stop/restart it multiple times in a session
+//just causes the browser to crash. :/
+if (window.NFC === undefined)
+{
+	try {
+		window.NFC = new NDEFReader
+	} catch(e) {
+		window.NFC = {
+			scan: () => {}
+		}
+	}
+}
+
+NFC.onreading = async event =>
+{
+	const res = await api(`
+	query ($rfid: String!) {
+		getBookByTag (rfid: $rfid) {
+			__typename
+			...on Book {
+				title
+				subtitle
+				authors
+				thumbnail
+				description
+			}
+			...on BookTagDoesNotExistError {
+				message
+			}
+			...on ApiFailedError {
+				message
+			}
+		}
+	}`)
+
+	if (res.__typename !== 'Book')
+	{
+		_.modal({
+			type: 'error',
+			title: 'ERROR',
+			text: res.message,
+			buttons: ['OK'],
+		}).catch(() => {})
+		return
+	}
+
+	_('book', res)
+}
+
+NFC.scan()
+
+window.unload.push(() => {
+	NFC.onreading = undefined
+})
