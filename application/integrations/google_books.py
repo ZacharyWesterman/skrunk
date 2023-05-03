f25c168c53841f64ccc769759e01ea93d522a2c3
Zachary Westerman <westerman.zachary@gmail.com>
2023-05-02 21:10:16 -0500

    remove google books API key dependency; it's not needed to just search books

--- application/integrations/google_books.py
+++ application/integrations/google_books.py
@@ -5,29 +5,7 @@ import json
 
 from . import exceptions
 
-_ENABLED = True
-try:
-	with open('data/secrets.json', 'r') as fp:
-		API_KEY = json.load(fp).get('google_books')
-		if API_KEY is None:
-			_ENABLED = False
-except FileNotFoundError:
-	_ENABLED = False
-
-if not _ENABLED:
-	def red(text):
-		return f'\033[91m{text}\033[0m'
-
-	def gray(text):
-		return f'\033[90m{text}\033[0m'
-
-	print( red('   WARNING: No google_books API key found in data/secrets.json!'))
-	print(gray('   Google Books integration will be disabled.'))
-
 def query(*, title: str = '', author: str = '') -> list:
-	if not _ENABLED:
-		return []
-
 	query_fields = []
 
 	t = title.replace(':', '').strip().replace(' ', '+')
@@ -41,7 +19,7 @@ def query(*, title: str = '', author: str = '') -> list:
 	text_query = '+'.join(query_fields)
 	response_fields = 'items(id,volumeInfo(authors,title,subtitle,description,industryIdentifiers,pageCount,categories,maturityRating,language,publisher,publishedDate,imageLinks))'
 
-	url = f'https://www.googleapis.com/books/v1/volumes?q={text_query}&key={API_KEY}&fields={response_fields}&orderBy=relevance&maxResults=20'
+	url = f'https://www.googleapis.com/books/v1/volumes?q={text_query}&fields={response_fields}&orderBy=relevance&maxResults=20'
 	response = requests.get(url)
 	if response.status_code != 200:
 		raise exceptions.ApiFailedError(f'Google Books API call failed with status code {response.status_code}: {response.text}')
@@ -63,7 +41,7 @@ def query(*, title: str = '', author: str = '') -> list:
 def get(*, id: str) -> dict:
 	response_fields = 'id,volumeInfo(authors,title,subtitle,description,industryIdentifiers,pageCount,categories,maturityRating,language,publisher,publishedDate,imageLinks)'
 
-	url = f'https://www.googleapis.com/books/v1/volumes/{id}?key={API_KEY}&fields={response_fields}'
+	url = f'https://www.googleapis.com/books/v1/volumes/{id}?fields={response_fields}'
 	response = requests.get(url)
 	if response.status_code != 200:
 		raise exceptions.ApiFailedError(f'Google Books API call failed with status code {response.status_code}: {response.text}')
