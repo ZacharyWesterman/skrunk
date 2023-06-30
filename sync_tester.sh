67b59e05b5c29767b50013ea57abe5d8d3c7c567
Zachary Westerman <westerman.zachary@gmail.com>
2023-06-29 21:40:47 -0500

    Split up resolvers into their respective schema files

--- sync_tester.sh
+++ sync_tester.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-inotifywait . -rm -e close_write | while read dir event file
-do
-	[ "$(<<<"$dir" grep git)" != "" ] && continue
-	path="$dir$file"
-	scp "$path" graphql:/home/ubuntu/server/"$path"
-done
+function inotif()
+{
+	inotifywait . -rm -e close_write | while read dir event file
+	do
+		[ "$(<<<"$dir" grep git)" != "" ] && continue
+		path="$dir$file"
+		scp "$path" tester:/home/tester/server/"$path"
+	done
+}
+
+ssh -oConnectTimeout=10 -n tester "cd /home/tester/server && git clean -fd && git pull" || exit 1
+inotif &
+
+ssh -t tester "
+	sudo systemctl restart server
+	trap 'sudo systemctl stop server; exit' INT
+	journalctl -f -u server
+"
