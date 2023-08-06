158fb968c88ef7a71a47ae0703adde6adcaf8c7f
Zachary Westerman <westerman.zachary@gmail.com>
2023-08-05 19:28:09 -0500

    bugfix

--- application/resolvers/query/settings.py
+++ application/resolvers/query/settings.py
@@ -1,6 +1,5 @@
 from application.db.settings import get_enabled_modules
 import application.db.perms as perms
 
-@perms.require(['admin'])
 def resolve_get_enabled_modules(_, info) -> list:
     return get_enabled_modules()
