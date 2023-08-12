ef46ecc892770597ea29dff6c674b41ba0dd4242
Zachary Westerman <westerman.zachary@gmail.com>
2023-08-12 15:51:16 -0500

    use correct range for more recent xkcd comics

--- application/routes/misc.py
+++ application/routes/misc.py
@@ -5,6 +5,6 @@ import random
 application = None
 
 def random_xkcd() -> Response:
-    comic_num = random.randint(0, 200)
+    comic_num = random.randint(1000, 2814)
     response = requests.get(f'https://xkcd.com/{comic_num}/info.0.json')
     return Response(response.text, response.status_code)
