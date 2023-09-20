4ef48898fdeaf934ed668e09b09d9c4e356a41e3
Zachary Westerman <westerman.zachary@gmail.com>
2023-09-19 22:29:48 -0500

    make preview images smaller so they load faster

--- application/db/blob.py
+++ application/db/blob.py
@@ -79,7 +79,7 @@ def save_blob_data(file: object, auto_unzip: bool, tags: list = []) -> list:
 		if blob['ext'].lower() in images.extensions():
 			this_blob_path = path(blob['id'], blob['ext'])
 			preview_path = preview(f'{blob["id"]}_p{blob["ext"]}')
-			if images.downscale(this_blob_path, 1024, preview_path):
+			if images.downscale(this_blob_path, 512, preview_path):
 				db.update_one({'_id': ObjectId(blob['id'])}, {'$set': {'preview': f'{blob["id"]}_p{blob["ext"]}'}})
 
 		elif blob['ext'].lower() in models.extensions():
