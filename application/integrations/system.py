d608d11ed80bf568630076256eb962df7de3a64e
Zachary Westerman <westerman.zachary@gmail.com>
2023-10-20 21:38:16 -0500

    Only display one 'Disk Usage' area if disks are on a single mount point

--- application/integrations/system.py
+++ application/integrations/system.py
@@ -1,5 +1,6 @@
 from application.db import blob
 import psutil
+from pathlib import Path
 
 def disk_usage() -> list[dict]:
 
@@ -12,5 +13,18 @@ def disk_usage() -> list[dict]:
 			'total': inf.total,
 		}
 
+	def get_mount_point(dir: str) -> str:
+		parent = Path(dir).resolve()
+		while str(parent) != '/' and not parent.is_mount():
+			parent = parent.parent
 
-	return [info('Application','.'), info('Blob Storage', blob.blob_path)] if blob.blob_path else [info('Application', '.')]
+		return str(parent)
+
+
+	if blob.blob_path:
+		if get_mount_point('.') == get_mount_point(blob.blob_path):
+			return [info('Application / Blob Storage', '.')]
+		else:
+			return [info('Application','.'), info('Blob Storage', blob.blob_path)]
+	else:
+		return [info('Application', '.')]
