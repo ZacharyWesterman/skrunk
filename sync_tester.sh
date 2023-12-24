b3038a08e7c7823b80a5de2ed6ca48df0a0a1ad2
Zachary Westerman <westerman.zachary@gmail.com>
2023-12-24 12:45:09 -0600

    Allow sync_tester to run in systems without inotify

--- sync_tester.sh
+++ sync_tester.sh
@@ -15,13 +15,45 @@ function inotif()
 	done
 }
 
+function simulate_inotify()
+{
+	declare -A mods
+	for i in $(git status --porcelain | cut -b 3-)
+	do
+		mods["$i"]=$(date +%s -r "$i")
+	done
+
+	while true
+	do
+		for i in $(git status --porcelain | cut -b 3-)
+		do
+			mod=$(date +%s -r "$i")
+			if [ "$mod" != "${mods["$i"]}" ]
+			then
+				mods["$i"]="$mod"
+				scp "$i" tester:/home/tester/server/"$i"
+			fi
+		done
+	done
+}
+
+function sync_files()
+{
+	if which inotifywait 2>/dev/null
+	then
+		inotif
+	else
+		simulate_inotify
+	fi
+}
+
 ssh -oConnectTimeout=10 -n tester "cd /home/tester/server && git checkout . && git clean -fd && git fetch origin && git checkout $(git branch | grep '*' | cut -b 2-) && git pull" || exit 1
 for i in $(git status --porcelain | cut -b 3-)
 do
 	scp "$i" tester:/home/tester/server/"$i"
 done
 
-inotif &
+sync_files
 
 ssh -t tester "
 	sudo systemctl restart server
