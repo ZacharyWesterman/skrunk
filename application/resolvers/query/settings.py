4cd52efe4ec9a8e003928e60a216426c9bc6902f
Zachary Westerman <westerman.zachary@gmail.com>
2023-12-29 00:31:16 -0600

    Don't allow non-admins to search all configs

--- application/resolvers/query/settings.py
+++ application/resolvers/query/settings.py
@@ -1,4 +1,5 @@
 from application.db.settings import get_enabled_modules, get_groups, get_all_configs, get_config
+from application.db import perms
 
 def resolve_get_enabled_modules(_, info) -> list:
     return get_enabled_modules()
@@ -6,6 +7,7 @@ def resolve_get_enabled_modules(_, info) -> list:
 def resolve_get_groups(_, info) -> list:
     return get_groups()
 
+@perms.require(['admin'])
 def resolve_get_all_configs(_, info) -> list:
     return get_all_configs()
 
