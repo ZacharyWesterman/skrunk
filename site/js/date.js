c3996986edfde8824642bfaec1c452de23487be9
Zachary Westerman <westerman.zachary@gmail.com>
2024-05-02 22:59:10 -0500

    Fix elapsed dates not playing nice around month boundaries : closes #206

--- site/js/date.js
+++ site/js/date.js
@@ -1,13 +1,4 @@
 window.date = {
-	//Precision formats
-	YEARS: 'year',
-	MONTHS: 'month',
-	WEEKS: 'week',
-	DAYS: 'day',
-	HOURS: 'hour',
-	MINUTES: 'minute',
-	SECONDS: 'second',
-	MILLISECONDS: 'millisecond',
 
 	months: {
 		abbrev: [
@@ -136,12 +127,10 @@ window.date = {
 	/**
 	 * Takes a date and returns a human-readable representation of the difference between the date and now
 	 *
-	 * @param {boolean} exact If true, output the exact difference (down to precision level). Otherwise output only highest non-zero difference.
-	 * @param {string} precision The level of precision to show in the diff. See possible values at top.
-	 * @param {boolean} include_zero Whether to include zero-diffs in the output.
+	 * @param {string|Date} date_obj The date to compare to the current system date.
 	 * @returns {string} The elapsed time between the date and now.
 	 */
-	elapsed: function(date_obj, exact = false, precision = date.SECOND, include_zero = false)
+	elapsed: function(date_obj)
 	{
 		let from = (typeof date_obj === 'string') ? new Date(date_obj) : date_obj
 		let to = new Date()
@@ -150,49 +139,26 @@ window.date = {
 		if (direction === 'in') from = [to, to = from][0] //swap from/to to keep diff positive
 
 		let output = []
-		const diff = method => to[method]() - from[method]()
+		const diff = to - from
 
-		const diffs = {
-			year: diff('getFullYear'),
-			month: diff('getMonth'),
-			week: Math.floor(diff('getDate') / 7),
-			day: (exact && (precision !== date.WEEKS)) ? diff('getDate') : (diff('getDate') % 7),
-			hour: diff('getHours'),
-			minute: diff('getMinutes'),
-			second: diff('getSeconds'),
-			millisecond: diff('getMilliseconds'),
-		}
+		const ratios = [
+			[1000 * 60 * 60 * 24 * 365, 'year'],
+			[1000 * 60 * 60 * 24 * 30, 'month'],
+			[1000 * 60 * 60 * 24 * 7, 'week'],
+			[1000 * 60 * 60 * 24, 'day'],
+			[1000 * 60 * 60, 'hour'],
+			[1000 * 60, 'minute'],
+			[5 * 1000, 'second'], //Ignore any timespan shorter than 5 seconds
+		]
 
-		const list = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second', 'millisecond']
-
-		//Make sure that all diffs are positive
-		const ratio = {
-			month: ['year', 12],
-			week: ['month', Math.ceil(date.months.days[to.getMonth()] / 7)],
-			day: (exact && (precision !== date.WEEKS)) ? ['month', date.months.days[to.getMonth()]] : ['week', 7],
-			hour: ['day', 24],
-			minute: ['hour', 60],
-			second: ['minute', 60],
-			millisecond: ['second', 1000],
-		}
-
-		for (const i of [...list].reverse())
+		for (const ratio of ratios)
 		{
-			if (diffs[i] < 0)
+			if (diff > ratio[0])
 			{
-				diffs[i] += ratio[i][1]
-				diffs[ratio[i][0]] -= 1
+				const amt = Math.floor(diff / ratio[0])
+				output.push(amt + ' ' + ratio[1] + (amt === 1 ? '' : 's'))
+				break
 			}
-		}
-
-		for (const i of list)
-		{
-			if (diffs[i] || include_zero)
-			{
-				output.push(diffs[i] + ' ' + i + ((diffs[i] === 1) ? '' : 's'))
-				if (!exact && diffs[i]) break
-			}
-			if (exact && i === precision) break
 		}
 
 		if (output.length === 0)
