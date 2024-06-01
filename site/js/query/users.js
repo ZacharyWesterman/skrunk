ab00be06de2e1257ec835bebb8a7d67c7500c01b
Zachary Westerman <westerman.zachary@gmail.com>
2024-06-01 17:22:14 -0500

    Fix user list sometimes not caching correctly

--- site/js/query/users.js
+++ site/js/query/users.js
@@ -1,9 +1,13 @@
 export default {
-	__user_list: null,
+	__user_list: {
+		all: null,
+		group: null,
+	},
 
 	list: async (filter, use_cache = true, restrict = true) => {
 		let res
-		if (query.users.__user_list === null || !use_cache) {
+		const sublist = restrict ? 'group' : 'all'
+		if (query.users.__user_list[sublist] === null || !use_cache) {
 			res = await api(`query ($restrict: Boolean!) {
 				listUsers (restrict: $restrict) {
 					username
@@ -12,10 +16,10 @@ export default {
 			}`, {
 				restrict: restrict,
 			})
-			query.users.__user_list = res
+			query.users.__user_list[sublist] = res
 		}
 		else
-			res = query.users.__user_list
+			res = query.users.__user_list[sublist]
 		return (filter ? res.filter(filter) : res).map(i => { return { value: i.username, display: i.display_name } })
 	},
 
