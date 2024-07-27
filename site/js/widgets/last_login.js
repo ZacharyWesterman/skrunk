b9a7fb950591b1a4caf9a5d6f23148484399040c
Zachary Westerman <westerman.zachary@gmail.com>
2024-07-27 15:26:06 -0500

    Allow clicking 'last login' widget to toggle between login date and elapsed time since login

--- site/js/widgets/last_login.js
+++ site/js/widgets/last_login.js
@@ -1,15 +1,23 @@
 export default async (config, field) => {
 	const date_format = environment.mobile ? date.short : date.output
-
-	const users = (await api(`{ listUsers (restrict: false) { username last_login } }`))
-		.map(user => {
-			const last_login = user.last_login ? date_format(user.last_login) : '<span class="disabled">Never</span>'
-			return `<tr><td>${user.username}</td><td>${last_login}</tr>`
-		})
-
+	const users = await api(`{ listUsers (restrict: false) { username last_login } }`)
 
 	const style = field.style
 	style['max-height'] = '220px'
 	style['overflow-y'] = 'scroll'
-	field.innerHTML = '<table>' + users.reduce((a, b) => a + b) + '</table>'
+
+	let exact = true
+
+	function toggle() {
+		const text = users.map(user => {
+			const last_login = user.last_login ? (exact ? date_format(user.last_login) : date.elapsed(user.last_login)) : '<span class="disabled">Never</span>'
+			return `<tr><td>${user.username}</td><td>${last_login}</tr>`
+		}).reduce((a, b) => a + b)
+
+		field.innerHTML = '<table>' + text + '</table>'
+		exact = !exact
+	}
+
+	field.onclick = toggle
+	toggle()
 }
