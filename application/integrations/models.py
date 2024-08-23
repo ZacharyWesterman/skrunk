567977130b68bfde11f8b2bf869ce8b2059af99e
Zachary Westerman <westerman.zachary@gmail.com>
2024-08-22 22:32:42 -0500

    Use different mesh conversion library to allow more up-to-date versions of Python to be used

--- application/integrations/models.py
+++ application/integrations/models.py
@@ -1,12 +1,15 @@
 __ALL__ = ['to_glb', 'extensions']
 
-from pathlib import Path
-from . import exceptions
-import aspose.threed as a3d
+import trimesh
 
 def extensions():
 	return ['3ds', '.3mf', '.amf', '.ase', '.rvm', '.dae', '.drc', '.dxf', '.fbx', '.gltf', '.obj', '.ma', '.mb', '.ply', '.jt', '.stl', '.u3d', '.usd', '.usdz', '.x']
 
 def to_glb(input_filename: str, output_filename: str) -> None:
-	scene = a3d.Scene.from_file(input_filename)
-	scene.save(output_filename)
+	mesh = trimesh.load(input_filename, force = 'mesh')
+
+	if not isinstance(mesh, trimesh.Trimesh):
+		print(f'MESH ERROR: Cannot load file as 3D mesh: {input_filename}')
+		return
+	
+	mesh.export(output_filename, file_type = 'glb')
