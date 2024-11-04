cad452677439148a5f5eaf46695f41b65bd056d2
Zachary Westerman <westerman.zachary@gmail.com>
2024-11-03 22:25:32 -0600

    Add monkey patch for json serialization of arbitrary objects

--- application/types/__init__.py
+++ application/types/__init__.py
@@ -1,1 +1,10 @@
+import json
+
+#Monkey patch JSON serialization so that objects are JSON serializable
+def wrapped_default(self, obj):
+    return obj.__dict__ if getattr(obj, '__dict__') else obj
+json.JSONEncoder.default = wrapped_default
+
+#Then import database objects
+
 from .blob_storage import *
