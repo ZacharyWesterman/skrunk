684f30aaa58fad3ba45a94cec33c43a57f61817e
Zachary Westerman <westerman.zachary@gmail.com>
2024-11-03 21:02:29 -0600

    Allow automatic dataclass-to-dict conversion from resolvers

--- application/resolvers/decorators.py
+++ application/resolvers/decorators.py
@@ -18,12 +18,20 @@ def handle_client_exceptions(func: Callable) -> Callable:
 		Callable: A wrapped function that handles `ClientError` exceptions.
 	"""
 
+	def dictify(obj):
+		if type(obj) is list:
+			return [dictify(i) for i in obj]
+		if type(obj) is dict:
+			return {key: dictify(val) for key, val in obj}
+		if is_dataclass(obj):
+			return { '__typename': obj.__class__.__name__, **asdict(obj) }
+
+		return obj
+
 	def wrapper(*args, **kwargs):
 		try:
-			result = func(*args, **kwargs)
+			return dictify(func(*args, **kwargs))
 		except ClientError as e:
 			return { '__typename': e.__class__.__name__, 'message': str(e) }
-
-		return { '__typename': result.__class__.__name__, **asdict(result) } if is_dataclass(result) else result
 
 	return wrapper
