a5f19418eef41d75815eba42ac11ee256d8a23cf
Zachary Westerman <westerman.zachary@gmail.com>
2024-11-04 12:09:59 -0600

    Handle implicit dict conversion of objects, if we choose to switch to dataclasses down the road

--- application/resolvers/decorators.py
+++ application/resolvers/decorators.py
@@ -17,20 +17,9 @@ def handle_client_exceptions(func: Callable) -> Callable:
 	Returns:
 		Callable: A wrapped function that handles `ClientError` exceptions.
 	"""
-
-	def dictify(obj):
-		if type(obj) is list:
-			return [dictify(i) for i in obj]
-		if type(obj) is dict:
-			return {key: dictify(val) for key, val in obj}
-		if is_dataclass(obj):
-			return { '__typename': obj.__class__.__name__, **asdict(obj) }
-
-		return obj
-
 	def wrapper(*args, **kwargs):
 		try:
-			return dictify(func(*args, **kwargs))
+			return func(*args, **kwargs)
 		except ClientError as e:
 			return { '__typename': e.__class__.__name__, 'message': str(e) }
 
--- application/types/__init__.py
+++ application/types/__init__.py
@@ -2,7 +2,7 @@ import json
 
 #Monkey patch JSON serialization so that objects are JSON serializable
 def wrapped_default(self, obj):
-    return obj.__dict__ if getattr(obj, '__dict__') else obj
+    return {'__typename': obj.__class__.__name__, **obj.__dict__} if getattr(obj, '__dict__') else obj
 json.JSONEncoder.default = wrapped_default
 
 #Then import database objects
