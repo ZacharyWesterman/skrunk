f72ad45790b6bc85e2e4db72b24686f1628be830
Zachary Westerman <westerman.zachary@gmail.com>
2024-12-26 23:05:21 -0600

    Use new submodule structure to list album info

--- application/resolvers/query/integrations.py
+++ application/resolvers/query/integrations.py
@@ -16,27 +16,27 @@ def resolve_search_subsonic(_, info, query: str, start: int, count: int) -> list
 	password = get_config('subsonic:password')
 
 	if SUBSONIC is None:
-		SUBSONIC = subsonic.Session(url, username, password)
+		SUBSONIC = subsonic.SubsonicClient(url, username, password)
 
 	try:
 		res = SUBSONIC.search(query)
-		if 'album' in res:
-			album_ids = [i['id'] for i in res['album']]
-			covers, tracks = SUBSONIC.get_all_cover_art(album_ids)
-
-			for i in res['album']:
-				if 'coverArt' in i:
-					i['coverArt'] = covers[i['id']]
-
-				i['tracks'] = [{
-					'id': a['id'],
-					'title': a['title'],
-					'duration': a.get('duration', -1),
-				} for a in tracks[i['id']]]
 
 		return {
 			'__typename': 'SubsonicSearch',
-			'album': res.get('album', []),
+			'album': [
+				{
+					'id': i.id,
+					'name': i.title,
+					'coverArt': i.cover,
+					'tracks': [
+						{
+							'id': a.id,
+							'title': a.title,
+							'duration': a.duration or -1,
+						} for a in i.songs
+					],
+				} for i in res.albums or []
+			],
 		}
 	except subsonic.SessionError as e:
 		return {'__typename': 'SubsonicError', 'message': str(e)}
