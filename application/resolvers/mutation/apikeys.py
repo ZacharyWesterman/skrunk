ab494effe1310db021559a8f69993af8ff37bd2e
Zachary Westerman <westerman.zachary@gmail.com>
2025-03-12 11:38:52 -0500

    Add documentation

--- application/resolvers/mutation/apikeys.py
+++ application/resolvers/mutation/apikeys.py
@@ -1,5 +1,6 @@
 """application.resolvers.mutation.apikeys"""
 
+from ariadne.types import GraphQLResolveInfo
 from application.db.apikeys import new_api_key, delete_api_key
 from application.db import perms
 from . import mutation
@@ -7,11 +8,34 @@ from . import mutation
 
 @mutation.field('createAPIKey')
 @perms.require('admin')
-def resolve_create_api_key(_, info, description: str, permissions: list[str]) -> str:
+def resolve_create_api_key(_, info: GraphQLResolveInfo, description: str, permissions: list[str]) -> str:
+	"""
+	Resolver function to create a new API key.
+
+	Args:
+		_ (Any): Placeholder.
+		info (GraphQLResolveInfo): Information about the GraphQL execution state.
+		description (str): A description for the new API key.
+		permissions (list[str]): A list of permissions to be associated with the new API key.
+
+	Returns:
+		str: The newly created API key.
+	"""
 	return new_api_key(description, permissions)
 
 
 @mutation.field('deleteAPIKey')
 @perms.require('admin')
-def resolve_delete_api_key(_, info, key: str) -> bool:
+def resolve_delete_api_key(_, info: GraphQLResolveInfo, key: str) -> bool:
+	"""
+	Resolver function to delete an API key.
+
+	Args:
+		_ (Any): Placeholder.
+		info (GraphQLResolveInfo): Information about the GraphQL execution state.
+		key (str): The API key to be deleted.
+
+	Returns:
+		bool: True if the API key was successfully deleted, False otherwise.
+	"""
 	return delete_api_key(key)
--- application/resolvers/query/apikeys.py
+++ application/resolvers/query/apikeys.py
@@ -1,5 +1,6 @@
 """application.resolvers.query.apikeys"""
 
+from ariadne.types import GraphQLResolveInfo
 from application.db.apikeys import get_api_keys
 from application.db import perms
 from . import query
@@ -7,5 +8,15 @@ from . import query
 
 @query.field('getAPIKeys')
 @perms.require('admin')
-def resolve_get_api_keys(_, info) -> list:
+def resolve_get_api_keys(_, info: GraphQLResolveInfo) -> list:
+	"""
+	Resolver function to get API keys.
+
+	Args:
+		_ (Any): Placeholder.
+		info (GraphQLResolveInfo): Information about the GraphQL execution state.
+
+	Returns:
+		list: A list of API keys.
+	"""
 	return get_api_keys()
