81061c3a63af19b7d72da19f9ffa50de6cc83214
Zachary Westerman <westerman.zachary@gmail.com>
2025-05-08 21:16:15 -0500

    Add related sites list widget : closes #288

---
+++ site/js/widgets/related_sites.js
@@ -0,0 +1,27 @@
+export default async (config, field) => {
+	const related_sites = (await api(`{ getConfig(name: "related_sites") }`))
+		.split('\n')
+		.map(i => {
+			try {
+				const info = JSON.parse(i)
+				return {
+					url: info[0],
+					title: info[1],
+					module: info[2],
+				}
+			} catch (e) {
+				return null
+			}
+		})
+		.filter(i => i !== null)
+		.filter(i => !i.module || !SelfUserData.disabled_modules.includes(i.module))
+
+	if (related_sites.length === 0) {
+		$.hide(field.parentElement)
+		return
+	}
+
+	field.innerHTML = related_sites.map(i => `<div style="text-align: center;">
+		<a href="${i.url}" target="_blank" class="related_site">${i.title}</a><br>
+	</div>`).join(' ')
+}
