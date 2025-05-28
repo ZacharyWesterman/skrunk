ce846f5d54fb6488069081bb1b3d193192a89283
Zachary Westerman <westerman.zachary@gmail.com>
2025-05-28 12:49:34 -0500

    Add optional book description field

--- application/types/book.py
+++ application/types/book.py
@@ -28,6 +28,8 @@ class Book(TypedDict):
 	publishedDate: datetime
 	## Whether the book has a description. If true, the description can be fetched using the getBookDescription query, but by default it is not included in the book data, to save bandwidth.
 	has_description: bool
+	## The description of the book, if available.
+	description: str | None
 	## A list of industry identifiers for the book, such as ISBN.
 	industryIdentifiers: list[BookIdent]
 	## The page count of the book.
