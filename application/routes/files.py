7eaa0718dd5ff4d1d54461ac483d4bce43332b16
Zachary Westerman <westerman.zachary@gmail.com>
2025-05-31 00:55:01 -0500

    simplify logic

--- application/routes/files.py
+++ application/routes/files.py
@@ -16,9 +16,6 @@ def read_file_data(path: str) -> Response:
 	try:
 		with open(path, 'rb') as fp:
 			mime = mimetypes.guess_type(path)
-			if mime:
-				return Response(fp.read(), 200, mimetype=mime[0])
-			else:
-				return Response(fp.read(), 200)
+			return Response(fp.read(), 200, mimetype=mime[0] if mime else None)
 	except FileNotFoundError:
 		return Response('File not found.', 404)
