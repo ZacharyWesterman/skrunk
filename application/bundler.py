98d14bcf426e0a055c384ec080d434f1f6068251
Zachary Westerman <westerman.zachary@gmail.com>
2026-05-02 22:44:35 -0500

    Add documentation

--- application/bundler.py
+++ application/bundler.py
@@ -1,3 +1,8 @@
+"""
+This module handles the (optional) bundling of site files, which can improve performance
+by decreasing the number of HTTP requests needed to load content.
+"""
+
 from pathlib import Path
 from jsmin import jsmin
 from rcssmin import cssmin
@@ -10,7 +15,15 @@ JS_FUNC_IMPORT = re.compile(r'\bimport *\( *[\'"]([^\'"]*)[\'"] *\)')
 
 
 def bundle_css(path: str) -> None:
-	with open(path, 'r') as fp:
+	"""
+	Resolve all imports in a CSS file and bundle into
+	a single (minified) monolithic file.
+
+	Args:
+		path (str): The path to the file.
+	"""
+
+	with open(path, 'r', encoding='utf8') as fp:
 		text = fp.read()
 
 	found = True
@@ -24,16 +37,24 @@ def bundle_css(path: str) -> None:
 			found = True
 
 		for (span, url) in reversed(insertions):
-			with open('site/' + url, 'r') as fp:
+			with open('site/' + url, 'r', encoding='utf8') as fp:
 				text = text[:span[0]] + fp.read() + text[span[1] + 1:]
 
-	with open('site/bundled/' + Path(path).name, 'w') as fp:
+	with open('site/bundled/' + Path(path).name, 'w', encoding='utf8') as fp:
 		# Minify the CSS before outputting
 		fp.write(cssmin(text))  # type: ignore
 
 
 def bundle_js(path: str) -> None:
-	with open(path, 'r') as fp:
+	"""
+	Resolve all imports in a JS file and bundle into
+	a single (minified) monolithic file.
+
+	Args:
+		path (str): The path to the file.
+	"""
+
+	with open(path, 'r', encoding='utf8') as fp:
 		text = fp.read()
 
 	# Replace statements like `import name from url` or `import url`
@@ -45,9 +66,13 @@ def bundle_js(path: str) -> None:
 		insertions += [(span, name, url)]
 
 	for (span, name, url) in reversed(insertions):
-		with open(str(Path(path).parent / url), 'r') as fp:
+		with open(str(Path(path).parent / url), 'r', encoding='utf8') as fp:
 			if name is not None:
-				imported_text = 'const ' + name + ' = (() => {\n' + JS_EXPORT.sub('return ', fp.read()) + '})()\n'
+				imported_text = (
+					'const ' + name + ' = (() => {\n' +
+					JS_EXPORT.sub('return ', fp.read()) +
+					'})()\n'
+				)
 			else:
 				imported_text = fp.read()
 
@@ -61,19 +86,27 @@ def bundle_js(path: str) -> None:
 		insertions += [(span, url)]
 
 	for (span, url) in reversed(insertions):
-		with open('site/' + url, 'r') as fp:
-			imported_text = '(async () => { return { default: (() => {\n' + JS_EXPORT.sub('return ', fp.read()) + '})() } })()'
+		with open('site/' + url, 'r', encoding='utf8') as fp:
+			imported_text = (
+				'(async () => { return { default: (() => {\n' +
+				JS_EXPORT.sub('return ', fp.read()) +
+				'})() } })()'
+			)
 			text = text[:span[0]] + imported_text + text[span[1]:]
 
-	with open('site/bundled/' + Path(path).name, 'w') as fp:
+	with open('site/bundled/' + Path(path).name, 'w', encoding='utf8') as fp:
 		# Minify the JS before outputting
 		fp.write(jsmin(text, quote_chars='\'"`'))
 
 
 def bundle() -> None:
+	"""
+	Bundle all relevant site files.
+	"""
+
 	print('Bundling source for performance...', end='', flush=True)
 	Path('site/bundled').mkdir(exist_ok=True)
-	with open('data/bundle_files.txt', 'r') as fp:
+	with open('data/bundle_files.txt', 'r', encoding='utf8') as fp:
 		for line in fp.readlines():
 			filename = line.strip()
 			ext = Path(filename).suffix
@@ -85,6 +118,10 @@ def bundle() -> None:
 
 
 def no_bundle() -> None:
+	"""
+	Remove any bundled files, if they exist.
+	"""
+
 	bundle_dir = Path('site/bundled')
 	if bundle_dir.exists():
 		for i in bundle_dir.iterdir():
@@ -93,5 +130,16 @@ def no_bundle() -> None:
 
 
 def get_bundled_path(path: str) -> str | None:
+	"""
+	Get the path of the bundled version of a file, if any.
+
+	Args:
+		path (str): The path of the non-bundled version of the file.
+
+	Returns:
+		str | None: The path of the bundled version of the file,
+			or None if no bundled version exists.
+	"""
+
 	bundled_path = Path('site/bundled') / Path(path).name
 	return str(bundled_path) if bundled_path.exists() else None
