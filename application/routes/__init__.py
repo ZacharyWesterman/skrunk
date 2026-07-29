8545705ef9e390d45611bf06b5da4f3dd7b81556
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-29 17:53:58 -0500

    add route for checking how quiescent the server is

--- application/routes/__init__.py
+++ application/routes/__init__.py
@@ -1,6 +1,33 @@
 """application.routes"""
 
+from datetime import UTC, datetime
+
+from flask import Response, jsonify
+
 from . import api, auth, blob, misc, site
+
+__last_query_at: datetime | None = None
+__running_query_ct: int = 0
+
+
+def tq(func):
+	"""
+	A decorator to track the last time a route was called, and how many responses are in-progress.
+
+	This decorator exists purely to know when it's safe to shut down/restart the server.
+	It doesn't track ANYTHING other than the quiescence of the back end.
+	"""
+	def wrapper(*args, **kwargs):
+		global __last_query_at, __running_query_ct
+		__last_query_at = datetime.now(UTC)
+		__running_query_ct += 1
+		result = func(*args, **kwargs)
+		__running_query_ct -= 1
+		return result
+
+	wrapper.__name__ = func.__name__
+
+	return wrapper
 
 
 def init(application) -> None:
@@ -16,25 +43,35 @@ def init(application) -> None:
 	site.application = application
 	blob.application = application
 
-	application.route('/auth', methods=['POST'])(auth.auth_user)
-	application.route('/auth/verify', methods=['POST'])(auth.verify_token)
-	application.route('/auth/request-reset-code', methods=['POST'])(auth.request_reset_code)
-	application.route('/auth/reset', methods=['POST'])(auth.reset_password)
+	application.route('/auth', methods=['POST'])(tq(auth.auth_user))
+	application.route('/auth/verify', methods=['POST'])(tq(auth.verify_token))
+	application.route('/auth/request-reset-code', methods=['POST'])(tq(auth.request_reset_code))
+	application.route('/auth/reset', methods=['POST'])(tq(auth.reset_password))
 
-	application.route('/api', methods=['POST'])(api.graphql)
-	application.route('/xkcd', methods=['GET'])(misc.random_xkcd)
+	application.route('/api', methods=['POST'])(tq(api.graphql))
+	application.route('/xkcd', methods=['GET'])(tq(misc.random_xkcd))
 
-	application.route('/', methods=['GET'])(site.main_page)
-	application.route('/<path:path>', methods=['GET'])(site.get)
-	application.route('/<path:path>.png', methods=['GET'])(site.get_icon)
-	application.route('/<path:path>.svg', methods=['GET'])(site.get_svg)
+	application.route('/', methods=['GET'])(tq(site.main_page))
+	application.route('/<path:path>', methods=['GET'])(tq(site.get))
+	application.route('/<path:path>.png', methods=['GET'])(tq(site.get_icon))
+	application.route('/<path:path>.svg', methods=['GET'])(tq(site.get_svg))
 
-	application.route('/blob/<path:path>', methods=['GET'])(blob.stream)
-	application.route('/download/<path:path>', methods=['GET'])(blob.download)
-	application.route('/preview/<path:path>', methods=['GET'])(blob.preview)
-	application.route('/upload', methods=['POST'])(blob.upload)
+	application.route('/blob/<path:path>', methods=['GET'])(tq(blob.stream))
+	application.route('/download/<path:path>', methods=['GET'])(tq(blob.download))
+	application.route('/preview/<path:path>', methods=['GET'])(tq(blob.preview))
+	application.route('/upload', methods=['POST'])(tq(blob.upload))
 
 	@application.after_request
 	def after_request(response):
 		response.headers.add('Accept-Ranges', 'bytes')
 		return response
+
+	# Quiescence reporting doesn't need an API key.
+	# Thinking is that there's nothing super valuable to
+	# be gained from knowing how active the server is currently.
+	@application.route('/quiescence', methods=['GET'])
+	def get_quiescence() -> Response:
+		return jsonify({
+			'last_query': __last_query_at,
+			'in_progress': __running_query_ct,
+		})
