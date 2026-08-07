c2e4905012edf1cc92b4034397e92cee3ca6bd49
Zachary Westerman <westerman.zachary@gmail.com>
2026-08-06 21:45:29 -0500

    add full support for remote video processing

--- application/integrations/videos.py
+++ application/integrations/videos.py
@@ -1,6 +1,7 @@
 """application.integrations.videos"""
 
 import subprocess
+import uuid
 from functools import cache
 from shutil import which
 
@@ -47,15 +48,115 @@ def create_low_res(video_path: str, output_video: str, hosts: list[str]) -> None
 	Returns:
 		None
 	"""
-	subprocess.run(
-		[
-			'ffmpeg', '-v', 'quiet', '-stats',
-			'-i', video_path,
-			'-vf', 'scale=480:-2,setsar=1:1',
-			output_video,
-		],
-		check=False
+
+	def run_local():
+		subprocess.run(
+			[
+				'ffmpeg', '-v', 'quiet', '-stats',
+				'-i', video_path,
+				'-vf', 'scale=480:-2,setsar=1:1',
+				output_video,
+			],
+			check=False
+		)
+
+	# If no hosts set in config, just run locally
+	if len(hosts) == 0:
+		run_local()
+		return
+
+	ideal_host = min(((i, get_normalized_load(i)) for i in hosts), key=lambda x: x[1])
+	localhost_load = get_normalized_load()
+
+	# If best host was a failure, just run locally
+	if ideal_host[1] == 9999:
+		run_local()
+		return
+
+	# If local load is 1/3rd or less, and the best host has a HIGHER load than 1/3rd, just run locally.
+	if localhost_load <= 0.3 and ideal_host[1] > 0.3:
+		run_local()
+		return
+
+	# All checks passed, run on best host
+	host = ideal_host[0]
+	jobid = uuid.uuid4()
+	from_ext = video_path.split('.')[-1]
+	to_ext = output_video.split('.')[-1]
+	from_file = f'/tmp/skrunk-conv-input-{jobid}.{from_ext}'
+	to_file = f'/tmp/skrunk-conv-output-{jobid}.{to_ext}'
+
+	# Transfer file up to host
+	subprocess.run([
+		'scp',
+		video_path,
+		f'{host}:{from_file}',
+	], check=False)
+
+	# Process the file
+	proc = subprocess.Popen(
+		['ssh', '-tt', host, f'ffmpeg -v quiet -stats -i {from_file} -vf scale=480:-2,setsar=1:1 {to_file}'],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
 	)
+	proc.communicate()
+
+	# Transfer file down from host
+	subprocess.run([
+		'scp',
+		f'{host}:{to_file}',
+		output_video,
+	], check=False)
+
+	# Clean up old files on host
+	proc = subprocess.Popen(
+		['ssh', '-tt', host, f'rm -f {from_file} {to_file}'],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+	)
+	proc.communicate()
+
+
+def get_normalized_load(host: str = 'localhost') -> float:
+	"""
+	Get a number from 0.0 to 1.0 indicating the percent load on the given host.
+
+	Args:
+		host (str): The SSH name of the machine, or `localhost` to check locally.
+
+	Returns:
+		float: `9999` if unable to calculate the load,
+			otherwise a number indicating the load if successfully calculated.
+	"""
+
+	try:
+		if host in ['', 'localhost']:
+			text = subprocess.check_output(['top', '-n', '1'])
+			nproc = subprocess.check_output(['nproc'])
+		else:
+			proc = subprocess.Popen(
+				['ssh', '-tt', host, '-oConnectTimeout=10', 'nproc; top -n 1'],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+			)
+			stdout, _ = proc.communicate()
+
+			if stdout is None:
+				return 9999
+
+			nproc, text = stdout.split(b'\n', 1)
+	except subprocess.CalledProcessError:
+		return 9999
+
+	text = text.split(b'\n', 1)[0]
+	nproc = int(nproc)
+
+	if text.find(b'load average: ') < 0:
+		return 9999
+
+	load = float(text.split(b'load average: ')[1].split(b',')[0])
+
+	return load / nproc
 
 
 @cache
@@ -73,16 +174,13 @@ def has_ffmpeg(host: str = 'localhost') -> bool:
 	if host in ['', 'localhost']:
 		return which('ffmpeg') is not None
 
-	try:
-		subprocess.check_call([
-			'ssh', host,
-			'-oConnectTimeout=10',
-			'which ffmpeg'
-		])
-	except subprocess.CalledProcessError:
-		return False
-
-	return True
+	proc = subprocess.Popen(
+		['ssh', '-tt', host, '-oConnectTimeout=10', 'which ffmpeg'],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+	)
+	proc.communicate()
+	return proc.returncode == 0
 
 
 def can_create_previews(hosts: list[str]) -> bool:
