75f98fa595a7f1964663729a5366bd4c799b175a
Zachary Westerman <westerman.zachary@gmail.com>
2026-08-06 20:03:14 -0500

    start adding support for processing videos on hosts other than locally

--- application/integrations/videos.py
+++ application/integrations/videos.py
@@ -1,6 +1,8 @@
 """application.integrations.videos"""
 
 import subprocess
+from functools import cache
+from shutil import which
 
 from imageio.v3 import imread, imwrite
 
@@ -33,13 +35,14 @@ def create_thumbnail_from_first_frame(video_path: str, output_image: str) -> Non
 	imwrite(output_image, first_frame)
 
 
-def create_low_res(video_path: str, output_video: str) -> None:
+def create_low_res(video_path: str, output_video: str, hosts: list[str]) -> None:
 	"""
 	Creates a low-res version of the given video.
 
 	Args:
 		video_path (str): The path to the video file.
 		output_video (str): The path where the output video will be saved.
+		hosts (list[str]): The list of remote hosts to process on rather than locally.
 
 	Returns:
 		None
@@ -53,3 +56,50 @@ def create_low_res(video_path: str, output_video: str) -> None:
 		],
 		check=False
 	)
+
+
+@cache
+def has_ffmpeg(host: str = 'localhost') -> bool:
+	"""
+	Check if the local machine or a given remote host is accessible and has ffmpeg.
+
+	Args:
+		host (str): The SSH name of the machine, or `localhost` to check locally.
+
+	Returns:
+		bool: True if the given machine has ffmpeg, False otherwise.
+	"""
+
+	if host in ['', 'localhost']:
+		return which('ffmpeg') is not None
+
+	try:
+		subprocess.check_call([
+			'ssh', host,
+			'-oConnectTimeout=10',
+			'which ffmpeg'
+		])
+	except subprocess.CalledProcessError:
+		return False
+
+	return True
+
+
+def can_create_previews(hosts: list[str]) -> bool:
+	"""
+	Check if either the local machine or any remote hosts can create video previews with ffmpeg.
+
+	Args:
+		hosts (list[str]): The list of remote hosts to process on rather than locally.
+
+	Returns:
+		bool: True if any hosts have ffmpeg, False otherwise.
+	"""
+	if has_ffmpeg():
+		return True
+
+	for i in hosts:
+		if has_ffmpeg(i):
+			return True
+
+	return False
