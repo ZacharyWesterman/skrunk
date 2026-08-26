492b4e6ed633e80dcc51e93bcca095956457e0fe
Zachary Westerman <westerman.zachary@gmail.com>
2026-08-26 14:26:16 -0500

    fix thumbnails sometimes getting rotated on creation : closes #370

--- application/integrations/images.py
+++ application/integrations/images.py
@@ -1,6 +1,6 @@
 """application.integrations.images"""
 
-from PIL import Image
+from PIL import ExifTags, Image
 
 
 def extensions() -> list:
@@ -31,6 +31,18 @@ def downscale(path: str, max_width: int, output_path: str) -> bool:
 
 	if width <= max_width:
 		return False  # No need to downscale
+
+	# JPEGs can have metadata indicating their rotation. Account for that
+	o = [i[0] for i in ExifTags.TAGS.items() if i[1] == 'Orientation']
+	if len(o):
+		exif = dict(image.getexif().items())
+		orientation = {
+			3: 180,
+			6: 270,
+			8: 90,
+		}.get(exif[o[0]])
+		if orientation is not None:
+			image = image.rotate(orientation, expand=True)
 
 	image.thumbnail((max_width, max_width), Image.Resampling.LANCZOS)
 
