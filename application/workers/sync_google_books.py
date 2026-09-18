938da94356375305efee12d521596d04bb7d06e1
Zachary Westerman <westerman.zachary@gmail.com>
2026-09-18 18:39:53 -0500

    allow google books sync to quietly fail

--- application/workers/sync_google_books.py
+++ application/workers/sync_google_books.py
@@ -3,6 +3,7 @@
 from datetime import UTC, datetime, timedelta
 
 from application.db.book import list_books_not_synced, sync_book_data
+from application.integrations.exceptions import ApiFailedError
 
 
 def sync_google_books() -> None:
@@ -12,4 +13,7 @@ def sync_google_books() -> None:
 	since = datetime.now(UTC) - timedelta(weeks=1)
 
 	for book_id in list_books_not_synced(since, 0, 10):
-		sync_book_data(book_id)
+		try:
+			sync_book_data(book_id)
+		except ApiFailedError:
+			pass
